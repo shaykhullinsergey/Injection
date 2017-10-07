2e5979fad037601556e5afcca697a30b34b6217c
yolorusher <yolorusher@gmail.com>
2017-10-07 15:44:57 +0300

    Add AppEntityState

---
+++ Shaykhullin.Injection/App/AppEntityState.cs
@@ -0,0 +1,10 @@
+﻿namespace Shaykhullin.Injection.App
+{
+  internal class AppEntityState<TRegister>
+  {
+    public IServiceBuilder Builder { get; set; }
+    public IDependencyContainer Container { get; set; }
+    public TRegister Returns { get; set; }
+    public string Named { get; set; }
+  }
+}
