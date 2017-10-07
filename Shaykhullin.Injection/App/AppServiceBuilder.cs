6108e7daa5e6af279d19dc623bf5c85002fca946
yolorusher <yolorusher@gmail.com>
2017-10-07 15:43:58 +0300

    Add AppServiceBuilder

---
+++ Shaykhullin.Injection/App/AppServiceBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Shaykhullin.Injection.App;
+using System;
+
+namespace Shaykhullin.Injection
+{
+  public class AppServiceBuilder : IServiceBuilder
+  {
+    private IDependencyContainer container;
+    public IService Service { get; }
+
+    public AppServiceBuilder()
+    {
+      container = new AppDependencyContainer();
+      Service = new AppService(container);
+
+      Register<IService>()
+        .Returns(Service)
+        .AsSingleton();
+    }
+
+    public IServiceEntity<TRegister> Register<TRegister>() =>
+      new AppServiceEntity<TRegister>(new AppEntityState<TRegister>
+      {
+        Builder = this,
+        Container = container
+      });
+  }
+}
