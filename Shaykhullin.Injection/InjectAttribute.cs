b62d44a5c22c645b515fc6254c6a3659f6abf14c
yolorusher <yolorusher@gmail.com>
2017-10-07 15:46:28 +0300

    Add InjectAttribute

---
+++ Shaykhullin.Injection/InjectAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shaykhullin.Injection
+{
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class InjectAttribute : Attribute
+  {
+    public string Name { get; set; }
+    public object[] Args { get; set; }
+
+    public InjectAttribute(params object[] args)
+    {
+      Args = args ?? throw new ArgumentNullException(nameof(args));
+    }
+  }
+}
