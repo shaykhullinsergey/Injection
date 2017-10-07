643ac5e36dbca6907255ac027064082fcaa5df37
yolorusher <yolorusher@gmail.com>
2017-10-07 15:45:55 +0300

    Add Creational Behaviours

---
+++ Shaykhullin.Injection/App/AppSingletonCreationalBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shaykhullin.Injection.App
+{
+  internal class AppSingletonCreationalBehaviour<TRegister> : ICreationalBehaviour
+  {
+    private object instance;
+
+    public AppSingletonCreationalBehaviour(TRegister returns, params object[] args)
+    {
+      instance = returns != null 
+        ? returns 
+        : Activator.CreateInstance(typeof(TRegister), args);
+    }
+
+    public TResolve Create<TResolve>(params object[] args)
+    {
+      return (TResolve)instance;
+    }
+  }
+}
---
+++ Shaykhullin.Injection/App/AppTransientCreationalBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Shaykhullin.Injection.App
+{
+  internal class AppTransientCreationalBehaviour<TRegister> : ICreationalBehaviour
+  {
+    private TRegister returns;
+
+    public AppTransientCreationalBehaviour(TRegister returns)
+    {
+      this.returns = returns;
+    }
+
+    public TResolve Create<TResolve>(params object[] args)
+    {
+      return (TResolve)(returns != null ? DeepCopy(returns) : Activator.CreateInstance(typeof(TRegister), args));
+    }
+
+    private TRegister DeepCopy(TRegister returns)
+    {
+      using (var ms = new MemoryStream())
+      {
+        var serializer = new XmlSerializer(typeof(TRegister));
+        serializer.Serialize(ms, returns);
+        ms.Position = 0;
+        return (TRegister)serializer.Deserialize(ms);
+      }
+    }
+  }
+}
