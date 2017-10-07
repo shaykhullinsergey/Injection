e869e7e8ce16fc87c36aa190f91014745bcb40e3
yolorusher <yolorusher@gmail.com>
2017-10-07 15:42:46 +0300

    Add Service

---
+++ Shaykhullin.Injection/Core/IService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shaykhullin.Injection
+{
+  public interface IService
+  {
+    TResolve Resolve<TResolve>(params object[] args);
+    TResolve Resolve<TResolve>(string named, params object[] args);
+    IEnumerable<TResolve> ResolveAll<TResolve>(params object[] args);
+  }
+}
