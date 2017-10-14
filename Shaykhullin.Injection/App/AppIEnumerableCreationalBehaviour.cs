49aac2b814658887185e70113eaaa040065061ad
yolorusher <yolorusher@gmail.com>
2017-10-14 11:56:05 +0300

    Version 2.3.2 variable namings

--- Shaykhullin.Injection/App/AppIEnumerableCreationalBehaviour.cs
+++ Shaykhullin.Injection/App/AppIEnumerableCreationalBehaviour.cs
@@ -4,33 +4,33 @@ using System.Collections.Generic;
 
 namespace Shaykhullin.Injection.App
 {
-    internal class AppIEnumerableCreationalBehaviour : ICreationalBehaviour
+  internal class AppIEnumerableCreationalBehaviour : ICreationalBehaviour
+  {
+    private readonly Type register;
+    private readonly IDependencyContainer container;
+
+    public AppIEnumerableCreationalBehaviour(Type register, IDependencyContainer container)
     {
-        private readonly Type memberType;
-        private readonly IDependencyContainer container;
-
-        public AppIEnumerableCreationalBehaviour(Type memberType, IDependencyContainer container)
-        {
-            this.memberType = memberType;
-            this.container = container;
-            MetaInfo = new AppMetaInfo<AppIEnumerableCreationalBehaviour>();
-        }
-        
-        public TResolve Create<TResolve>(object[] args)
-        {
-            var argType = memberType.GetGenericArguments()[0];
-
-            var listType = typeof(List<>).MakeGenericType(argType);
-            var myList = (IList)Activator.CreateInstance(listType);
-
-            foreach (var c in container.GetAll(argType))
-            {
-                myList.Add(Utils.Resolve<object>(container, c));
-            }
-
-            return (TResolve)myList;
-        }
-
-        public IMetaInfo MetaInfo { get; }
+      this.register = register;
+      this.container = container;
+      MetaInfo = new AppMetaInfo<AppIEnumerableCreationalBehaviour>();
     }
+
+    public TResolve Create<TResolve>(object[] args)
+    {
+      var argType = register.GetGenericArguments()[0];
+
+      var myList = (IList)Activator.CreateInstance(
+        typeof(List<>).MakeGenericType(argType));
+
+      foreach (var c in container.GetAll(argType))
+      {
+        myList.Add(Utils.Resolve<object>(container, c, args));
+      }
+
+      return (TResolve)myList;
+    }
+
+    public IMetaInfo MetaInfo { get; }
+  }
 }
